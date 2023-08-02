9f0c80914caf50fda31c824682afde909089047f
vineeth16081994 <58562792+vineeth16081994@users.noreply.github.com>
2023-08-02 18:42:15 +0530

    Create Selenium.java

---
+++ src/main/java/com/qa/opencart/pages/Selenium.java
@@ -0,0 +1,12 @@
+package com.qa.opencart.pages;
+
+import org.openqa.selenium.By;
+
+public class Selenium {
+	
+	
+	private By emailId=By.id("selenium");
+	private By ordrid=By.id("api");
+     //just a git file
+
+}
