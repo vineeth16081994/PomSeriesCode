278598e77bd9b01953ee0f9ac29ef5f2960106a1
vineeth16081994 <58562792+vineeth16081994@users.noreply.github.com>
2023-08-04 14:24:12 +0530

    Update Mail.java

--- src/main/java/com/qa/opencart/pages/Mail.java
+++ src/main/java/com/qa/opencart/pages/Mail.java
@@ -4,16 +4,14 @@ public class Mail
     public void sendMail()
       {
 
-      
-
-  
-
-      System.out.println("mail sent vineeth");
+     System.out.println("mail sent vineeth");
       }
     public void getMail()
     {
     System.out.println("mail received vineeth");
     }
+
+    //i am venkat
 
 
   }
