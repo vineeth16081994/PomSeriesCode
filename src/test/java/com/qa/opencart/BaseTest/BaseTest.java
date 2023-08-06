f65163361ea5f0a3f90559b13b6e883ae1889013
Vineeth Sargant <vineethsargant@Vineeths-MacBook-Air.local>
2023-08-06 14:34:57 +0530

    removed unwanted

--- src/test/java/com/qa/opencart/BaseTest/BaseTest.java
+++ src/test/java/com/qa/opencart/BaseTest/BaseTest.java
@@ -3,9 +3,9 @@ package com.qa.opencart.BaseTest;
 import java.util.Properties;
 
 import org.openqa.selenium.WebDriver;
-import org.testng.annotations.AfterMethod;
+
 import org.testng.annotations.AfterTest;
-import org.testng.annotations.BeforeMethod;
+
 import org.testng.annotations.BeforeTest;
 import org.testng.asserts.SoftAssert;
 
